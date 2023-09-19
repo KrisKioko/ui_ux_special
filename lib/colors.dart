46cd6a14e740164310a264589ce29d5e838463ad
KrisKioko <chris.kioko20@gmail.com>
2023-09-19 16:17:47 +0300

    Scheduler App showing flutter motions

---
+++ lib/colors.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+class ReplyColors {
+  static Color white50 = const Color(0xFFFFFFFF);
+
+  static Color black800 =  const Color(0xFF121212);
+  static Color black900 = const Color(0xFF000000);
+  
+  static Color blue50 = const Color(0xFFEEF0F2);
+  static Color blue100 = const Color(0xFFD2DBE0);
+  static Color blue200 = const Color(0xFFADBBC4);
+  static Color blue300 = const Color(0xFF8CA2AE);
+  static Color blue600 = const Color(0xFF4A6572);
+  static Color blue700 = const Color(0xFF344955);
+  static Color blue800 = const Color(0xFF232F34);
+
+  static Color orange300 = const Color(0xFFFBD790);
+  static Color orange400 = const Color(0xFFF9BE64);
+  static Color orange500 = const Color(0xFFF9AA33);
+
+  static Color red200 = const Color(0xFFCF7779);
+  static Color red400 = const Color(0xFFFF4C5D);
+
+  static Color white50Alpha060 = const Color(0x99EEF0F2);
+
+  static Color blue50Alpha060 = const Color(0x99EEF0F2);
+
+  static Color black900Alpha020 = const Color(0x33000000);
+  static Color black900Alpha087 = const Color(0xDE000000);
+  static Color black900Alpha060 = const Color(0x99000000);
+
+  static Color greyLabel = const Color(0xFFAEAEAE);
+  static Color darkBottomAppBarBackground = const Color(0xFF2D2D2D);
+  static Color darkDrawerBackground = const Color(0xFF353535);
+  static Color darkCardBackground = const Color(0xFF1E1E1E);
+  static Color darkChipBackground = const Color(0xFF2A2A2A);
+  static Color lightChipBackground = const Color(0xFFE5E5E5);
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:ui_ux_special/app.dart';
+
+
+void main() {
+  runApp(const ReplyApp());
+}
