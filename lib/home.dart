9c5b64acf2a1d6a57d454ed18858523c3a1e0209
KrisKioko <chris.kioko20@gmail.com>
2023-09-21 10:44:26 +0300

    Updating Flutter Motions

--- lib/home.dart
+++ lib/home.dart
@@ -297,6 +297,163 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      extendBody: true,
+      body: LayoutBuilder(
+        builder: _buildStack,
+      ),
+      bottomNavigationBar: _AnimatedBottomAppBar(
+        bottomAppBarController: _bottomAppBarController,
+        bottomAppBarCurve: _bottomAppBarCurve,
+        bottomDrawerVisible: _bottomDrawerVisible,
+        drawerController: _drawerController,
+        dropArrowCurve: _dropArrowCurve,
+        toggleBottomDrawerVisibility: _toggleBottomDrawerVisibility,
+      ),
+      floatingActionButton: _bottomDrawerVisible ? null : const Padding(
+        padding: EdgeInsetsDirectional.only(bottom: 8),
+        child: _ReplyFab(),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _AnimatedBottomAppBar extends StatelessWidget {
+  const _AnimatedBottomAppBar({
+    required this.bottomAppBarController,
+    required this.bottomAppBarCurve,
+    required this.bottomDrawerVisible,
+    required this.drawerController,
+    required this.dropArrowCurve,
+    required this.toggleBottomDrawerVisibility,
+  });
+
+  final AnimationController bottomAppBarController;
+  final Animation<double> bottomAppBarCurve;
+  final bool bottomDrawerVisible;
+  final AnimationController drawerController;
+  final Animation<double> dropArrowCurve;
+  final VoidCallback toggleBottomDrawerVisibility;
+
+  @override
+  Widget build(BuildContext context) {
+    var fadeOut = Tween<double>(begin: 1, end: -1).animate(
+      drawerController.drive(CurveTween(curve: standardEasing)),
+    );
+
+    return Selector<EmailStore, bool>(
+      selector: (context, emailStore) => emailStore.onMailView,
+      builder: (context, onMailView, child) {
+        bottomAppBarController.forward();
+
+        return SizeTransition(
+          sizeFactor: bottomAppBarCurve,
+          axisAlignment: -1,
+          child: Padding(
+            padding: const EdgeInsetsDirectional.only(top: 2),
+            child: BottomAppBar(
+              shape: const WaterfallNotchedRectangle(),
+              notchMargin: 6,
+              child: Container(
+                color: Colors.transparent,
+                height: kToolbarHeight,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      onTap: toggleBottomDrawerVisibility,
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 16),
+
+                          RotationTransition(
+                            turns: Tween(
+                              begin: 0.0,
+                              end: 1.0,
+                            ).animate(dropArrowCurve),
+                            child: Icon(
+                              Icons.arrow_drop_up,
+                              color: ReplyColors.white50,
+                            ),
+                          ),
+
+                          const SizedBox(width: 8),
+
+                          const _ReplyLogo(),
+
+                          const SizedBox(width: 10),
+
+                          // TODO: Add Fade through transition between disappearing mailbox title (Motion)
+
+                          onMailView ? const SizedBox(width: 48) : FadeTransition(
+                            opacity: fadeOut,
+                            child: Selector<EmailStore, String>(
+                              selector: (context, emailStore) => emailStore.currentlySelectedInbox,
+                              builder: (context, currentlySelectedInbox, child) {
+                                return Text(
+                                  currentlySelectedInbox,
+                                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                    color: ReplyColors.white50,
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Expanded(
+                      child: Container(
+                        color: Colors.transparent,
+                        child: _BottomAppBarActionItems(
+                          drawerController: drawerController,
+                          drawerVisible: bottomDrawerVisible,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }, 
+    );
+  }
+}
+
+class _BottomAppBarActionItems extends StatelessWidget {
+  const _BottomAppBarActionItems({required this.drawerController, required this.drawerVisible});
+  
+
+  final AnimationController drawerController;
+  final bool drawerVisible;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<EmailStore>(
+      builder: (context, model, child) {
+        final onMailView = model.onMailView;
+        var radius = const Radius.circular(12);
+        final modalBorder = BorderRadius.only(
+          topRight: radius,
+          topLeft: radius,          
+        );
+
+        Color? starIconColor;
+
+        if (onMailView) {
+          var currentEmailStarred = false;
+
+          if (model.emails[model.currentlySelectedInbox]!.isNotEmpty) {
+            
+          }
+        }
+      },
+    );
   }
 }
